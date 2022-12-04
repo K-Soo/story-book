70cf61bd8070130dfe74a1d0eeabbfbd1db8d3ac
K-Soo <kanabun102@naver.com>
2022-12-04 18:53:00 +0900

    feat(bookstorywritesearchcontainer.tsx): 도서 정보 불러오기 API 연동 및 데이터 바인딩

--- pages/book-story/[idx]/index.tsx
+++ pages/book-story/[idx]/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import type { NextPage } from 'next';
-import BookDetailContainer from '@containers/bookDetailContainer';
 import BookStoryDetailContainer from '@containers/bookStoryDetailContainer';
 import Layout from '@components/layout';
 import DetailLayout from '@components/layout/detailLayout';
 
-export default function BookStoryDetailPage(props: NextPage) {
+export default function BookStoryDetailPage() {
   return <BookStoryDetailContainer />;
 }
 
--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -4,3 +4,7 @@ import AuthContainer from '@containers/authContainer';
 export default function SignUpPage() {
   return <AuthContainer />;
 }
+
+SignUpPage.getLayout = (page: React.ReactElement) => {
+  return <>{page}</>;
+};
